7361952a50f309b02cb64fe44087d1839dcb2520
joonhoswe <joonhoswe@gmail.com>
2024-04-25 23:25:32 -0400

    initial builds

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,20 @@ const App = () => {
   return (
     <div className="bg-gray-900 text-white flex justify-center items-center w-full h-screen overflow-hidden relative p-4">
       <div className="absolute inset-0 flex justify-between items-center p-4">
-        <div className="z-10">
-          <h1 className="text-3xl lg:text-4xl xl:text-5xl font-bold">Hello There!</h1>
-          <p>Some text on the left</p>
-        </div>
-        <div className="z-10">
-          <h2 className="text-2xl lg:text-3xl xl:text-4xl font-bold">Jonathan Oh</h2>
-          <p>A bit about Jonathan</p>
+      <div className="z-10 ml-36">
+          <h2 className="text-5xl font-bold">Hi, I'm Jonathan</h2>
           <button className="bg-blue-600 text-white rounded-full px-6 py-2 mt-4">Learn More</button>
         </div>
+
+        <div className="z-10 flex flex-col space-y-5 mr-36">
+          <h1 className="text-3xl font-bold"> About me </h1>
+          <p className="text-s"> I'm a student interested in the intersection of <br></br> tech and law, planning to pursue patent law! </p>
+          <h1 className="text-3xl font-bold">Education</h1>
+          <p className="text-m"> Computer Science + Philosophy, Politics, and Law </p>
+        </div>
+        
       </div>
-      <img src={me} alt="Profile" className="w-96 h-96 z-0" />
+      <img src={me} alt="Profile" className="w-3/5 h-full z-0" />
     </div>
   );
 };
