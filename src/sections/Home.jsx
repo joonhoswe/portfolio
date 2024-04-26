615025aabd39d7a0a09fce35f725a5eccec40534
joonhoswe <joonhoswe@gmail.com>
2024-04-26 17:03:24 -0400

    typewriter effect

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from '../components/navbar';
+import {ReactTyped} from 'react-typed';
 import me from '../assets/me2.png';
 
 const Home = () => {
@@ -8,16 +9,37 @@ const Home = () => {
     <Navbar/>
     <div className=" text-white flex justify-center items-center overflow-hidden relative">
       <div className="absolute inset-0 flex justify-between items-center">
-      <div className="z-10 ml-36">
-          <h2 className="text-5xl font-bold">Hi, I'm Jonathan</h2>
+      <div className="z-10 ml-36 space-y-4">
+          <h2 className="text-5xl font-bold">Hi, I'm Jonathan,</h2>
+          <h2 className="text-5xl font-bold">
+          <ReactTyped
+              strings = {[
+                  "A Student",
+                  "An Inventor",
+                  "An Amatuer Chef ",
+              ]}
+              typeSpeed = {50}
+              backSpeed = {50}
+              loop
+          />
+          </h2>
           <button className="bg-blue-600 text-white rounded-full px-6 py-2 mt-4">Learn More</button>
         </div>
 
-        <div className="z-10 flex flex-col space-y-5 mr-28 text-white">
-          <h1 className="text-3xl font-bold"> About me </h1>
-          <p className="text-s"> I'm a student interested in the intersection of <br></br> tech and law, planning to pursue patent law! </p>
-          <h1 className="text-3xl font-bold">Education</h1>
-          <p className="text-m"> Computer Science + Philosophy, Politics, and Law </p>
+        <div className="z-10 flex flex-col space-y-5 mr-28">
+          <div>
+            <h1 className="text-3xl font-bold"> About me </h1>
+            <hr class="h-0.5 bg-gray-200 border-0 mb-3"></hr>
+            <p className="text-s text-gray-300"> I'm a student interested in the intersection of <br></br> tech and law, planning to pursue patent law! </p>
+          </div>
+          
+          <div>
+            <h1 className="text-3xl font-bold">Education</h1>
+            <hr class="h-0.5 bg-gray-200 border-0 mb-3"></hr>
+            <p className="text-m text-gray-300"> Emory University </p>
+            <p className="text-m text-gray-300"> Computer Science + Philosophy, Politics, and Law </p>
+          </div>
+          
         </div>
         
       </div>
