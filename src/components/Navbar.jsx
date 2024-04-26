a2b1a00e608fabedd4f97756846107c00c3f40a9
joonhoswe <joonhoswe@gmail.com>
2024-04-26 00:02:49 -0400

    building navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <div className="bg-transparent text-white flex justify-center items-center w-full">
-        <h1 className="text-xl"> Jonathan </h1>
+    <div className="flex justify-center items-center w-full h-14 sticky top-0 bg-gray-900 bg-opacity-20 backdrop-filter backdrop-blur-lg text-white">
+      <div>
+        <h1 className="text-xl">Jonathan</h1>
+      </div>
     </div>
   );
 };
